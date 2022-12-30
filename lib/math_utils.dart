e76f1b4620d594da3efcd92f146c29b22e00db29
Chinmay-Kalmane <kalmane3101@gmail.com>
2022-12-30 12:21:10 +0530

    First Commit

---
+++ lib/math_utils.dart
@@ -0,0 +1,11 @@
+library math_utils;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  int add(int value1, int value2) => value1 +value2;
+
+  //
+}
